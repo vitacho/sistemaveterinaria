3689433310518e4d1b0df40a9cec142a8747ebab
victor <DELL@DESKTOP-0PBI841.mshome.net>
2021-02-13 17:15:19 -0500

    mejora del modelos de detalle factura y factura

--- proyectoPrueba/src/Modelo/Detallefactura.java
+++ proyectoPrueba/src/Modelo/Detallefactura.java
@@ -5,10 +5,85 @@
  */
 package Modelo;
 
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
 /**
  *
  * @author DELL
  */
 public class Detallefactura {
-    
+
+    private int id_detallefactura;
+    private int cantidad;
+    private String detalle;
+    private double precio_unitario;
+    private double precio_total;
+    private Factura factura;
+    private Servicio servicio;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public int getId_detallefactura() {
+        return id_detallefactura;
+    }
+
+    public void setId_detallefactura(int id_detallefactura) {
+        this.id_detallefactura = id_detallefactura;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public String getDetalle() {
+        return detalle;
+    }
+
+    public void setDetalle(String detalle) {
+        this.detalle = detalle;
+    }
+
+    public double getPrecio_unitario() {
+        return precio_unitario;
+    }
+
+    public void setPrecio_unitario(double precio_unitario) {
+        this.precio_unitario = precio_unitario;
+    }
+
+    public double getPrecio_total() {
+        return precio_total;
+    }
+
+    public void setPrecio_total(double precio_total) {
+        this.precio_total = precio_total;
+    }
+
+    @ManyToOne
+    public Factura getFactura() {
+        return factura;
+    }
+
+    public void setFactura(Factura factura) {
+        this.factura = factura;
+    }
+
+    @ManyToOne
+    public Servicio getServicio() {
+        return servicio;
+    }
+
+    public void setServicio(Servicio servicio) {
+        this.servicio = servicio;
+    }
+
 }
---
+++ proyectoPrueba/src/Modelo/Factura.java
@@ -0,0 +1,87 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+
+/**
+ *
+ * @author DELL
+ */
+@Entity
+public class Factura implements Serializable {
+
+    private int id_factura;
+    private int nro_factura;
+    private Date fecha;
+    private double iva;
+    private Persona persona;
+    private List<Detallefactura> detallefactura = new ArrayList<Detallefactura>(0);
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public int getId_factura() {
+        return id_factura;
+    }
+
+    public void setId_factura(int id_factura) {
+        this.id_factura = id_factura;
+    }
+
+    public int getNro_factura() {
+        return nro_factura;
+    }
+
+    public void setNro_factura(int nro_factura) {
+        this.nro_factura = nro_factura;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public double getIva() {
+        return iva;
+    }
+
+    public void setIva(double iva) {
+        this.iva = iva;
+    }
+
+    @ManyToOne
+    public Persona getPersona() {
+        return persona;
+    }
+
+    public void setPersona(Persona persona) {
+        this.persona = persona;
+    }
+
+    @OneToMany(mappedBy = "factura", cascade = CascadeType.ALL)
+    public List<Detallefactura> getDetallefactura() {
+        return detallefactura;
+    }
+
+    public void setDetallefactura(List<Detallefactura> detallefactura) {
+        this.detallefactura = detallefactura;
+    }
+
+}
