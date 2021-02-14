a0b1cb532f0897fd6062a8ecbca87b8b3c8d0e08
XavierVill2525 <Jose.villavicencio@unl.edu.ec>
2021-02-13 21:20:41 -0500

    Consulta y Hospitalizacion

---
+++ proyectoPrueba/src/Modelo/Consulta.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Darley
+ */
+public class Consulta {
+
+    private int id;
+    private String presion;
+    private int temp;
+    private double peso;
+    private String motivo;
+    private String diagnostico;
+    private String veterinario;
+    private Mascota mascota;
+    private Date fecha;
+
+    
+    
+}
---
+++ proyectoPrueba/src/Modelo/Hospitalizacion.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Darley
+ */
+public class Hospitalizacion {
+
+    private int id;
+    private String diagnostico;
+    private String motivo;
+    private int temp;
+    private String sangrado;
+    private String conbulcion;
+    private String intoxicacion;
+    private String probleResp;
+    private String inchazon;
+    private Date ingreso;
+    private Date salida;
+    private String vereterinario;
+    private String estado;
+    private Mascota mascota;
+
+    
+    
+}
