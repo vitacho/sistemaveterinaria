1a88b39e59772055a95363a80559b4489032c4a4
Jennifer <Personal@DESKTOP-KSFE0T6>
2021-02-13 22:24:14 -0500

    actualizcion de modelo receta

--- proyectoPrueba/src/Modelo/Receta.java
+++ proyectoPrueba/src/Modelo/Receta.java
@@ -8,11 +8,13 @@ package Modelo;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 /**
  *
@@ -68,7 +70,7 @@ import javax.persistence.ManyToOne;
     public void setNum_receta(int num_receta) {
         this.num_receta = num_receta;
     }
-@ManyToOne
+  @OneToMany(mappedBy = "servicio", cascade = CascadeType.ALL)
     public List<Consulta> getListaConsultas() {
         return ListaConsultas;
     }
